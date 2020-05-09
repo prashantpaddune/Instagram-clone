a3c8a20c63bf422d7b69362f530bccf11b509c5c
prashantpaddune <elonpras@my.smccd.edu>
2020-05-09 23:17:34 +0530

    frontend: login page

--- frontend/src/components/screens/Login.js
+++ frontend/src/components/screens/Login.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import {Link} from "react-router-dom";
 
 function Login() {
     return (
-        <div>
-            <h1>login</h1>
+        <div className="mycard">
+            <div className="card auth-card input-field">
+                <h1 className="hotelfont">Instagram</h1>
+                <input
+                    type="text"
+                    placeholder="email"
+                />
+                <input
+                    type="password"
+                    placeholder="password"
+                />
+                <button className="btn waves-effect waves-light #64b5f6 blue darken-1">
+                    Login
+                </button>
+                <h6>
+                    <Link to="/signup">Don't have an account ?</Link>
+                </h6>
+                <h6>
+                    <Link to="/reset">Forgot password ?</Link>
+                </h6>
+
+            </div>
         </div>
     );
 }
