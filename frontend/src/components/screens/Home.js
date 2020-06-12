d79bebe750d483c81ffbcd855d0ec63fd0cdc2f4
prashantpaddune <elonpras@my.smccd.edu>
2020-06-12 22:31:34 +0530

    show user post on home page and my profile posts

--- frontend/src/components/screens/Home.js
+++ frontend/src/components/screens/Home.js
@@ -1,22 +1,165 @@
-import React from 'react';
+import React,{useState,useEffect,useContext} from 'react'
+import {UserContext} from '../../App'
+import {Link} from 'react-router-dom'
+const Home  = () => {
+    const [data,setData] = useState([])
+    const {state,dispatch} = useContext(UserContext)
+    useEffect(() => {
+        fetch('/allpost',{
+            headers: {
+                "Authorization":"Bearer "+localStorage.getItem("jwt")
+            }
+        })
+            .then(res => res.json())
+            .then(result => {
+                console.log(result)
+                setData(result.posts)
+            })
+    },[])
 
-function Home() {
+    const likePost = (id) => {
+        fetch('/like',{
+            method:"put",
+            headers:{
+                "Content-Type":"application/json",
+                "Authorization":"Bearer "+localStorage.getItem("jwt")
+            },
+            body:JSON.stringify({
+                postId:id
+            })
+        }).then(res => res.json())
+            .then(result => {
+                //   console.log(result)
+                const newData = data.map(item => {
+                    if (item._id === result._id){
+                        return result
+                    } else {
+                        return item
+                    }
+                })
+                setData(newData)
+            })
+            .catch(err => console.log(err))
+    }
+    const unlikePost = (id) => {
+        fetch('/unlike',{
+            method:"PUT",
+            headers:{
+                "Content-Type":"application/json",
+                "Authorization":"Bearer "+localStorage.getItem("jwt")
+            },
+            body:JSON.stringify({
+                postId:id
+            })
+        }).then(res => res.json())
+            .then(result => {
+                //   console.log(result)
+                const newData = data.map(item => {
+                    if (item._id === result._id){
+                        return result
+                    } else {
+                        return item
+                    }
+                })
+                setData(newData)
+            }).catch(err => console.log(err))
+    }
+
+    const makeComment = (text,postId) => {
+        fetch('/comment',{
+            method:"PUT",
+            headers:{
+                "Content-Type":"application/json",
+                "Authorization":"Bearer "+localStorage.getItem("jwt")
+            },
+            body:JSON.stringify({
+                postId,
+                text
+            })
+        })
+            .then(res => res.json())
+            .then(result => {
+                console.log(result)
+                const newData = data.map(item => {
+                    if(item._id === result._id){
+                        return result
+                    } else {
+                        return item
+                    }
+                })
+                setData(newData)
+            }).catch(err => console.log(err))
+    }
+
+    const deletePost = (postid) => {
+        fetch(`/deletepost/${postid}`,{
+            method:"delete",
+            headers:{
+                Authorization:"Bearer "+localStorage.getItem("jwt")
+            }
+        })
+            .then(res => res.json())
+            .then(result => {
+                console.log(result)
+                const newData = data.filter(item => {
+                    return item._id !== result._id
+                })
+                setData(newData)
+            })
+    }
     return (
         <div className="home">
-            <div className="card home-card">
-                <h6 style={{padding:"5px"}}>Prashant Paddune</h6>
-                <div className="card-image">
-                <img alt="" src="https://images.unsplash.com/photo-1557762820-f16cbda96afa?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"/>
-                </div>
-                <div className="card-content">
-                    <i className="material-icons" style={{color:"red"}}>favorite</i>
-                    <h6>Title</h6>
-                    <p>Amazing post</p>
-                    <input type="text" placeholder="add a comment" />
-                </div>
-            </div>
+            {
+                data.map(item => {
+                    return(
+                        <div className="card home-card" key={item._id}>
+                            <h5 style={{padding:"5px"}}><Link to={item.postedBy._id !== state._id?"/profile/"+item.postedBy._id :"/profile"  }>{item.postedBy.name}</Link> {item.postedBy._id === state._id
+                            && <i className="material-icons" style={{
+                                float:"right"
+                            }}
+                                  onClick={()=>deletePost(item._id)}
+                            >delete</i>
+
+                            }</h5>
+                            <div className="card-image">
+                                <img alt="" src={item.photo}/>
+                            </div>
+                            {/*<div className="card-content">*/}
+                            {/*    <i className="material-icons" style={{color:"red"}}>favorite</i>*/}
+                            {/*    {item.likes.includes(state._id)*/}
+                            {/*        ?*/}
+                            {/*        <i className="material-icons"*/}
+                            {/*           onClick={()=>{unlikePost(item._id)}}*/}
+                            {/*        >thumb_down</i>*/}
+                            {/*        :*/}
+                            {/*        <i className="material-icons"*/}
+                            {/*           onClick={()=>{likePost(item._id)}}*/}
+                            {/*        >thumb_up</i>*/}
+                            {/*    }*/}
+                            {/*    <h6>{item.likes.length} likes</h6>*/}
+                            {/*    <h6>{item.title}</h6>*/}
+                            {/*    <p>{item.body}</p>*/}
+                            {/*    {*/}
+                            {/*        item.comments.map(record=>{*/}
+                            {/*            return(*/}
+                            {/*                <h6 key={record._id}><span style={{fontWeight:"500"}}>{record.postedBy.name}</span> {record.text}</h6>*/}
+                            {/*            )*/}
+                            {/*        })*/}
+                            {/*    }*/}
+                            {/*    <form onSubmit={(e)=>{*/}
+                            {/*        e.preventDefault()*/}
+                            {/*        makeComment(e.target[0].value,item._id)*/}
+                            {/*    }}>*/}
+                            {/*        <input type="text" placeholder="add a comment" />*/}
+                            {/*    </form>*/}
+                            {/*</div>*/}
+                        </div>
+                    )
+                })
+            }
         </div>
     )
 }
 
-export default Home;
+
+export default Home
