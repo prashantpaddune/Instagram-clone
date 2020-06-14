72434c84a1e98610a14cacc1f12b700077399e33
prashantpaddune <elonpras@my.smccd.edu>
2020-06-14 14:41:22 +0530

    change ports

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const mongoose  = require('mongoose')
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 3003
 const {MONGOURI} = require('./config/keys')
 
 
